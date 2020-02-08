44b9bf343a5de70ab36c68dc6548753a4735aece
SamuelLabrador <sam.labrador@yahoo.com>
2020-02-08 10:17:17 -0800

    Modified the way the secret key is generated. Modified for production version. Requirements updated for production.

--- has/settings.py
+++ has/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,14 +21,25 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ipa@4@4h9975*kbdpx=hu&u9g5d2c7&1-mp4i414%cect-3#&c'
+# Try to import secret key
+try:
+    from .secret_key import SECRET_KEY
+except ImportError:
+    key = get_random_secret_key()
+    
+    SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(SETTINGS_DIR, 'secret_key.py')
+    
+    f = open(path, "w")
+    f.write("SECRET_KEY = '{}'".format(key))
+    f.close()
+
+    from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
