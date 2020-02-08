58d3377f11043d3cfea9c687e0dd74f95b444794
SamuelLabrador <sam.labrador@yahoo.com>
2020-02-08 14:19:26 -0800

    added additional unit tests

--- restapp/tests.py
+++ restapp/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+import json
+from cctv.models import *
 
 # Create your tests here.
+
+class RestAppTestCase(TestCase):
+	def setUp(self):
+		CCTV.objects.create(
+			district=1,
+			longitude=1,
+			latitude=1,
+			elevation=1,
+		)
+
+	def test_request(self):
+		factory = APIRequestFactory()
+		response = self.client.get('/api/cctv/')
+
+		cctv = json.loads(json.dumps(response.data))[0]
+		
+		self.assertTrue(cctv['latitude'] == 1)
+		self.assertTrue(cctv['longitude'] == 1)
+		
