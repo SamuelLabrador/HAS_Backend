ec9eb2ea35f8efa3d4ce56cbabb6303ea370c4e2
Takbir Islam <tisla002@ucr.edu>
2020-02-08 14:13:41 -0800

    enabled pagination with search

--- restapp/views.py
+++ restapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
-
+from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CCTVSerializers, PhotoSerializers, SearchSerializers
 from cctv.models import CCTV,Photo
 
 # Create your views here.
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
 class CCTVViewSet(viewsets.ModelViewSet):
 
     queryset = CCTV.objects.all()
@@ -17,45 +22,9 @@ class PhotoViewSet(viewsets.ModelViewSet):
     serializer_class = PhotoSerializers
 
 class SearchViewSet(viewsets.ModelViewSet):
-
-    #queryset = Photo.objects.all()
     
+    queryset = Photo.objects.all()
     serializer_class = SearchSerializers
-
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
-
     search_fields = ['=cctv__id',]
-
-    def get_queryset(self):
-        #num = self.request.query_params.get('num')
-        
-        
-
-        #if not num:
-        #    qset = Photo.objects.all()
-        #else:
-        #    qset = Photo.objects.all()[:int(num)]
-
-        #queryset = qset
-
-        #q = Photo.objects.all()
-        #queryset = q[:int(num)]
-
-        #queryset = Photo.objects.filter(pk__in = Photo.objects.all()[:int(num)+1].values_list('pk'))
-        #queryset = Photo.objects.all()[:int(num):1]
-
-        
-        num = self.request.query_params.get('num')
-        search = self.request.query_params.get('search')
-        print(search)
-
-        query = Photo.objects.all()
-        
-        if not num:
-            queryset = query
-        else:
-            #queryset = Photo.objects.filter(Photo.objects.all()[:int(num)+1])
-            queryset = query.filter(cctv__exact=int(search))[:int(2)]
-
-        return queryset
-    
+    pagination_class = StandardResultsSetPagination
