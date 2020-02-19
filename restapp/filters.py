5a24af573de85be622dc79bf75a3a734459890c6
Takbir Islam <tisla002@ucr.edu>
2020-02-19 00:09:51 -0800

    working on adding list of values to be searched, WIP

---
+++ restapp/filters.py
@@ -0,0 +1,35 @@
+import django_filters
+from django_filters import rest_framework as filters
+from django_filters.fields import Lookup
+
+from cctv.models import CCTV
+
+class MultiValueCharFilter(filters.BaseCSVFilter, filters.CharFilter):
+    def filter(self, qs, value):
+        # value is either a list or an 'empty' value
+        values = value or []
+
+        for value in values:
+            qs = super(MultiValueCharFilter, self).filter(qs, value)
+
+        return qs
+
+
+class CCTVFilter(django_filters.FilterSet):
+    county = MultiValueCharFilter(field_name='county', lookup_expr='contains')
+
+    class Meta:
+        model = CCTV
+        fields = ['county',]
+
+# class ListFilter(Filter):
+#     def filter(self, qs, value):
+#         value_list = value.split(u',')
+#         return super(ListFilter, self).filter(qs, Lookup(value_list, 'in'))
+
+# class ProductFilterSet(django_filters.FilterSet):
+#     county = ListFilter(field_name='county')
+
+#     class Meta:
+#         model = CCTV
+#         fields = ['county',]
--- restapp/views.py
+++ restapp/views.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CCTVSerializers, SearchSerializers
 from cctv.models import CCTV,Photo
-# from .filters import ProductFilterSet
+from .filters import CCTVFilter
 
 # Create your views here.
 class StandardResultsSetPagination(PageNumberPagination):
@@ -16,10 +16,10 @@ class CCTVViewSet(viewsets.ModelViewSet):
 
     queryset = CCTV.objects.all()
     serializer_class = CCTVSerializers
-    #filter_backends = (ProductFilterSet)
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filter_class = CCTVFilter
     #search_fields = ['=county',]
-    filterset_fields = ['county',]
+    #filterset_fields = ['county',]
 
 class SearchViewSet(viewsets.ModelViewSet):
     
