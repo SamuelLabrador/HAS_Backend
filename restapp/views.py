08692e9a2b1a0f65af9b31fe4d59735a588b1891
Takbir Islam <tisla002@ucr.edu>
2020-02-18 23:42:35 -0800

    added filtering for cctvs via county

--- restapp/views.py
+++ restapp/views.py
@@ -4,6 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CCTVSerializers, SearchSerializers
 from cctv.models import CCTV,Photo
+# from .filters import ProductFilterSet
 
 # Create your views here.
 class StandardResultsSetPagination(PageNumberPagination):
@@ -15,6 +16,10 @@ class CCTVViewSet(viewsets.ModelViewSet):
 
     queryset = CCTV.objects.all()
     serializer_class = CCTVSerializers
+    #filter_backends = (ProductFilterSet)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    #search_fields = ['=county',]
+    filterset_fields = ['county',]
 
 class SearchViewSet(viewsets.ModelViewSet):
     
