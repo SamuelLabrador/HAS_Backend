1eecfa2072354cbc862736830fadcd719d277835
SamuelLabrador <sam.labrador@Yahoo.com>
2020-02-28 15:39:46 -0800

    Adding base data pipeline class

---
+++ datapipeline/pipeline.py
@@ -0,0 +1,21 @@
+import tensorflow
+
+class Pipeline():
+	def __init__(self):
+		pass
+
+	# Starts data pipeline	
+	def start(self):
+		pass
+
+	def scrape_images(self):
+		pass
+
+	def index_images(self):
+		pass
+
+	def classify_images(self):
+		pass
+
+	def index_classifications(self):
+		pass
