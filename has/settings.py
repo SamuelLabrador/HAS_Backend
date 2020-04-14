99b2f4c24391f0fde9e64e8cff07dbba27e7369c
samuellabrador <samuel.labrador.cs@gmail.com>
2020-04-14 11:34:13 -0700

    Updated the settings to include cache

--- has/settings.py
+++ has/settings.py
@@ -115,12 +115,23 @@ if PRODUCTION_FLAG:
             'PORT': 5432,
         }
     }
+    CACHES = {
+	    'default': {
+	        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+	        'LOCATION': '127.0.0.1:11211',
+	    }
+	}
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+    }
+    CACHES = {
+    	'default': {
+        	'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    	}
     }
 
 # Password validation
