1f1223dce7eace72854ee08c735859335d6581a4
SamuelLabrador <sam.labrador@yahoo.com>
2020-04-14 12:28:25 -0700

    Set the hostname of the caching server. Managed by docker-compose

--- has/settings.py
+++ has/settings.py
@@ -118,7 +118,7 @@ if PRODUCTION_FLAG:
     CACHES = {
 	    'default': {
 	        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-	        'LOCATION': '127.0.0.1:11211',
+	        'LOCATION': 'cache:11211',
 	    }
 	}
 else:
