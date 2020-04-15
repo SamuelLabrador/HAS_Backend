39eb914b429c663f5a91fc48d73d33bec507b0cf
SamuelLabrador <sam.labrador@Yahoo.com>
2020-04-14 21:35:40 -0700

    updating the /image/ api

--- cctv/views.py
+++ cctv/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.conf import settings
 
-from .utilities import decode_path
+from .utilities import decode_path, generate_hash
 
 # Create your views here.
 def cctv(request):
@@ -11,7 +11,7 @@ def cctv(request):
 def image(request):
 	uri = request.build_absolute_uri()
 	hash_start = uri.rfind('/')
-	file_hash = uri[hash_start + 1:]
+	file_hash = generate_hash(uri[hash_start + 1:])
 
 	path = decode_path(settings.IMAGE_URL, file_hash) + '.jpg'
 
